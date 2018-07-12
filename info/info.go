6600f6880a4bea388403e35639a5cae4c01abf28
rafaelcastro141 <rafaelgyncastro63@gmail.com>
2018-07-12 18:09:21 -0300

    Função handler e ListenProxy em info.go

--- info/info.go
+++ info/info.go
@@ -6,22 +6,117 @@ import (
 	"net"
 	"fmt"
 	"gowebproxy/log"
+	"sort"
 )
+
+type Resource struct{
+	Name string
+	Size int
+}
+
+type ResourceCount struct{
+	Name string
+	Count int
+}
+
+type HostCount struct{
+	Host string
+	Count int
+}
 
 type Stats struct {
 	LastHostsVisited []string
-	LastResourceVisited []string
-	CountActiveConn int
+	LastResourceVisited []Resource
+	ActiveConn int
 	StartTime time.Time
 }
 
-func handler(conn net.Conn, statChan chan Stats){
-	// espera por uma resposta do servidor proxy
+type List struct{
+	HostsVisited []string
+	ResourceVisited []Resource 
+	StartTime time.Time
+	CountActiveConn int
+}
 
+var memory List
+
+func handler(conn net.Conn, statChan chan Stats){
+	defer conn.Close()
+
+	//Map para descobrir quantas vezes um host foi visitado
+	mpHost := make(map[string]int)
+	//Iterando para obter quantas vezes cada host foi visitado
+	for _, host := range memory.HostsVisited {
+		mpHost[host] += 1
+	}
+
+	//Array para preparar as estatística sobre o número de visitas em cada host
+	var hostsStatistic []HostCount
+	for k, v := range mpHost {
+		hostsStatistic = append(hostsStatistic, HostCount{k, v})
+	}
+
+	//Map para cada objeto associar com seu tamanho
+	mpResourceSize := make(map[string]int)
+	//Map para cada objeto contar o número de vezes que ele foi requisitado
+	mpResource := make(map[string]int)
+
+	for _, resource := range memory.ResourceVisited {
+		mpResourceSize[resource.Name] = resource.Size
+		mpResource[resource.Name] += 1
+	}
+
+	//Array para guardar para cada objeto requisitado seu tamanho
+	var resourceStatisticSize []Resource
+	for k, v := range mpResourceSize {
+		resourceStatisticSize = append(resourceStatisticSize, Resource{k, v})
+	}
 	
+	//Array para guardar para cada objeto o número de vezes que ele foi requisitado
+	var resourceStatistic []ResourceCount
+	for k, v := range mpResource{
+		resourceStatistic = append(resourceStatistic, ResourceCount{k, v})
+	}
+
+	//Agoro ordeno minhas estatísticas
+	sort.Slice(hostsStatistic, func(i, j int) bool{//Coloco os hosts com maiores números de requisições primeiro
+		return hostsStatistic[i].Count > hostsStatistic[j].Count
+	})
+
+	sort.Slice(resourceStatistic, func(i, j int) bool{//Coloco os objetos com maiores números de requisições primeiro
+		return resourceStatistic[i].Count > resourceStatistic[j].Count
+	})
+
+	sort.Slice(resourceStatisticSize, func(i, j int) bool{//Coloco os objetos de maior tamanho primeiro
+		return resourceStatisticSize[i].Size > resourceStatisticSize[j].Size;
+	})
+}
+
+func ListenProxy(statChan chan Stats){
+	for{
+		// Espera por uma resposta do servidor proxy
+		st := <-statChan
+
+		// Atualizo o número de conexões ativas
+		memory.CountActiveConn += st.ActiveConn
+
+		// Atualizo o tempo da conexão
+		if time.Time.IsZero(st.StartTime) == false {
+			memory.StartTime = st.StartTime
+		}
+		
+		// Adiciono os últimos hosts requisitados na lista
+		memory.HostsVisited = append(memory.HostsVisited, st.LastHostsVisited...)
+		
+		// Adiciono os últimos objetos requisitados na lista
+		memory.ResourceVisited = append(memory.ResourceVisited, st.LastResourceVisited...)
+	}
 }
 
 func InfoServer(port int, statChan chan Stats) {
+	// esperar respostas do servidor proxy
+	go ListenProxy(statChan)
+
 	host := ":" + strconv.Itoa(port)
 	// cria socket tcp na porta port
 	listen, err := net.Listen("tcp", host)
@@ -44,7 +139,7 @@ func InfoServer(port int, statChan chan Stats) {
 			log.PrintError(err)
 		} else {
 			// se nao houver erro, tratar conexao em outra goroutine
-			go handler(conn)
+			go handler(conn, statChan)
 		}
 	}	
 }
